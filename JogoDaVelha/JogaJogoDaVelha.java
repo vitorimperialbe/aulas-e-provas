682c8c1a9ee2ab285fe3117391f2aec0adc8154b
Vitorimperialbe <vitorimperialbertolli@gmail.com>
2025-04-17 16:29:52 -0300

    Update JogaJogoDaVelha.java

--- JogoDaVelha/JogaJogoDaVelha.java
+++ JogoDaVelha/JogaJogoDaVelha.java
@@ -5,37 +5,34 @@ public class JogaJogoDaVelha {
     public static void main(String[] args) {
         System.out.println("Bem-vindo ao teste do Jogo da Velha!");
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Digite 1 para jogar ou 0 para sair: ");
         int opcao = scanner.nextInt();
-
-        while (opcao != 0) {
-            System.out.println("\nDigite um número inteiro para a dimensão do tabuleiro: ");
+        Random random = new Random();
+        while (opcao!=0) {
+            System.out.println("Digite um número inteiro para a dimensão do tabuleiro: ");
             int dimensao = scanner.nextInt();
-
             JogoDaVelha jogo = new JogoDaVelha(dimensao);
-            Random random = new Random();
-
-            while (jogo.vencedor() == 0) {
-                int i = random.nextInt(dimensao);
-                int j = random.nextInt(dimensao);
+            int i, j;
+            while (jogo.vencedor() == 2) {
+                i = random.nextInt(dimensao);
+                j = random.nextInt(dimensao);
                 try {
                     jogo.poePeca(i, j);
                 } catch (IllegalArgumentException e) {
-                    continue; // Ignorar exceções e tentar novamente
+                    System.out.println(e.getMessage());
+                    continue;
                 }
-                System.out.println();
+//                System.out.println(jogo.toString());
             }
-
             if (jogo.vencedor() == 1) {
-                System.out.println("\nX ganhou!");
+                System.out.println("Jogador X venceu!");
             } else if (jogo.vencedor() == -1) {
-                System.out.println("\nO ganhou!");
+                System.out.println("Jogador O venceu!");
             } else {
-                System.out.println("\nEmpate!");
+                System.out.println("Empate!");
             }
-            System.out.println(jogo);
-            System.out.println("\nDeseja jogar novamente? [1] - sim, [0] - não: ");
+            System.out.println(jogo.toString());
+            System.out.println("Deseja jogar novamente? (1-sim, 0-nao): ");
             opcao = scanner.nextInt();
         }
     }
